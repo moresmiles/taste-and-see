5df98a1698cbc18e1675990c08c4cb627830684e
github username <github email address>
2018-07-16 12:17:19 +0300

    authenticate user before action, and skip before action on create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user
+  skip_before_action :authenticate_user, only: [:create]
   before_action :set_user, only: [:show, :update, :destroy]
 
   # GET /users
