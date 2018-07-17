297483cade3a81b89757d8bce9b4d8db8b3202fb
github username <github email address>
2018-07-17 16:49:31 +0300

    logged in and out routes and map state to props

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Welcome from '../components/Welcome';
 
 class App extends Component {
   render() {
@@ -13,9 +14,33 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+    const {isAuthenticated, user} = this.props
+    const loggedOut = (
+      <div>
+        <Route exact path="/" component={Welcome} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
       </div>
+    )
+    const loggedIn = (
+      <div>
+      </div>
+    )
+    return (
+      <Router>
+        <div className="App">
+        </div>
+      </Router>
     );
   }
 }
 
 export default App;
+const mapStateToProps = (state) => {
+  return {
+      isAuthenticated: state.user.isAuthenticated,
+      user: state.user.currentUser
+  }
+}
+
+export default connect(mapStateToProps, {})(App);
