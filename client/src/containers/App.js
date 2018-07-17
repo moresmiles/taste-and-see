add4a445f3345bb4bfc9d049a8ed235170617d6d
github username <github email address>
2018-07-17 17:12:04 +0300

    old App.js code removal

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux'
 import '../App.css';
@@ -12,15 +10,6 @@ import Login from './Login'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
     const {isAuthenticated, user} = this.props
     const loggedOut = (
       <div>
@@ -48,7 +37,6 @@ class App extends Component {
   }
 }
 
-export default App;
 const mapStateToProps = (state) => {
   return {
       isAuthenticated: state.user.isAuthenticated,
