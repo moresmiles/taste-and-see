ddf78363b4200c7b6365cf72b401df651a3da54a
github username <github email address>
2018-07-17 16:58:08 +0300

    user_token and find_user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :users
+  post 'user_token' => 'user_token#create'
+  post 'find_user' => 'users#find'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
