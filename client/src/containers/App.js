bd15fb3b2a3313ba17b7a627379cd9fb7cc9dd71
github username <github email address>
2018-07-18 17:43:47 +0300

    added routes for /newbeer and /beerlist

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -6,21 +6,27 @@ import Welcome from '../components/Welcome';
 import UserShow from '../components/UserShow';
 import Signup from './Signup';
 import Navigation from './Navigation'
+import BeerForm from './BeerForm';
+import BeersList from '../components/BeersList';
+
 import Login from './Login'
+//import '../beer.jpg'
 
 class App extends Component {
   render() {
     const {isAuthenticated, user} = this.props
     const loggedOut = (
       <div>
-        <Route exact path="/" component={Welcome} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={Welcome} />
       </div>
     )
     const loggedIn = (
       <div>
         <Route path="/account" render={() => <UserShow user={user} />} />
+        <Route path="/newbeer" component={BeerForm} />
+        <Route path="/beers" component={BeersList} />
       </div>
     )
     return (
