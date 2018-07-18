a87ad720bcef664fca98b6a2686b59aaf1d888da
github username <github email address>
2018-07-18 17:35:45 +0300

    added links to new beer and beers index page

--- client/src/components/UserShow.js
+++ client/src/components/UserShow.js
@@ -1,14 +1,16 @@
 import React from 'react';
-
+import { Link } from "react-router-dom";
 const UserShow = ({user}) => {
 
-    return (
-        <div>
-          <h2>You are logged in YAY!</h2>
-          <p>ID: {user.id} </p>
-          <p>email: {user.email} </p>
-        </div>
+  return (
+      <div>
+        <h2>You are logged in YAY!</h2>
+        <p>ID: {user.id} </p>
+        <p>email: {user.email} </p>
+        <p><Link to="/newbeer">New Beer</Link> </p>
+        <p><Link to="/beer" >Saved Beers</Link></p>
+      </div>
     );
-}
+  }
 
 export default UserShow;
