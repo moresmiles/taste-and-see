41ec93b142edbd61ab4a4c6329ee3c9a4f51c358
github username <github email address>
2018-07-18 18:07:46 +0300

    added LOADING_BEERS and FETCH_BEERS

--- client/src/reducers/beerReducer.js
+++ client/src/reducers/beerReducer.js
@@ -1,5 +1,9 @@
 export default ( state={ loading: false, beers: [] }, action ) => {
   switch( action.type ) {
+    case 'LOADING_BEERS':
+      return {...state, loading: true}
+    case 'FETCH_BEERS':
+      return {...state, beers: action.payload, loading: false}
     case 'CREATE_BEER':
       return {...state, beers: state.beers.concat(action.payload)}
     default:
