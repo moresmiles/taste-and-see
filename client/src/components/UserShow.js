88070116512d212603694a43b6a0f8f11853edb0
github username <github email address>
2018-07-18 18:04:01 +0300

    componentDidMount for fetchBeers

--- client/src/components/UserShow.js
+++ client/src/components/UserShow.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-const UserShow = ({user}) => {
+import { fetchBeers } from '../actions/beerActions';
+import { connect } from 'react-redux';
 
-  return (
+ class UserShow extends Component {
+   constructor(props){
+     super(props)
+
+     this.state = {
+       beers: []
+     }
+   }
+
+   componentDidMount() {
+     this.props.fetchBeers();
+    }
+
+ render(){
+   return (
       <div>
         <h2>You are logged in YAY!</h2>
         <p>ID: {user.id} </p>
         <p>email: {user.email} </p>
         <p><Link to="/new-beer">New Beer</Link> </p>
-        <p><Link to="/beer" >Saved Beers</Link></p>
+        <p><Link to="/beer" >Saved Beers</Link> </p>
       </div>
     );
   }
-
-export default UserShow;
+}
+export default connect(null, {fetchBeers})(UserShow);
