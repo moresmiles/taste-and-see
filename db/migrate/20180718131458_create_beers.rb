cd301f1c7fb3ebfe09c340c8edc1a1d9323dc8fc
github username <github email address>
2018-07-18 16:24:21 +0300

    migrated beers table, and seeds

--- db/migrate/20180718131458_create_beers.rb
+++ db/migrate/20180718131458_create_beers.rb
@@ -1,7 +1,11 @@
 class CreateBeers < ActiveRecord::Migration[5.1]
   def change
     create_table :beers do |t|
-
+      t.string :name
+      t.string :style
+      t.integer :rating
+      t.string :notes
+      t.integer :user_id
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(email:"testuser@test.com", password:"testuser", password_confirmation:"testuser")
+Beer.create(name:"Heady Topper", style:"Double IPA", rating: 5, notes:"...", user_id: 1 )
+Beer.create(name:"Sip of Sunshine", style:"IPA", rating: 5, notes:"...", user_id: 1 )
+Beer.create(name:"Bhindi", style:"IPA", rating: 4, notes:"...", user_id: 1 )
+Beer.create(name:"Boston Lager", style:"Lager", rating: 3, notes:"...", user_id: 1 )
