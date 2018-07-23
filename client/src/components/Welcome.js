9befe822611b715f2d23fa9b508f9c00837b60b9
github username <github email address>
2018-07-23 13:13:20 +0300

    just beers not wines

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import Button from 'antd/lib/button';
+import { Button } from 'antd';
 
 const Welcome = ({match}) => {
 
     return (
         <div id="welcome">
-            <h2>Welcome to Taste and See, a place to store all the delicious beers and wines that you've tried.</h2>
-            <p>Please <Button href="/signup" >Sign Up</Button> or <Button href="/login" >Log In</Button> to start </p>
+            <h2>Welcome to Taste and See, a place to store all the delicious beers that you've tried.</h2>
+            <p>Please <Button> <Link to="/signup" >Sign Up</Link></Button> or <Button><Link to="/login" >Log In</Link></Button> to start </p>
         </div>
     );
 }
