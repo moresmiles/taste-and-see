2e43f247b2f7f59b1f72511dfce5a650075d49e0
github username <github email address>
2018-07-25 14:29:58 +0300

    updateBeer function

--- client/src/actions/beerActions.js
+++ client/src/actions/beerActions.js
@@ -3,53 +3,77 @@ import fetch from 'isomorphic-fetch';
 const API_URL = "http://localhost:3001"
 
 export function fetchBeers() {
-    return (dispatch) => {
-        dispatch({type: 'LOADING_BEERS'})
-        return fetch(`${API_URL}/beers.json`)
-        .then(response => response.json())
-        .then(beers => dispatch({ type: 'FETCH_BEERS', payload: beers }));
-    }
+  return (dispatch) => {
+    dispatch({type: 'LOADING_BEERS'})
+    return fetch(`${API_URL}/beers.json`)
+    .then(response => response.json())
+    .then(beers => dispatch({ type: 'FETCH_BEERS', payload: beers }));
+  }
 }
 
 export function createBeer(beerDetails) {
-    return dispatch => {
-        return fetch(`${API_URL}/beers`, {
-            method: 'POST',
-            body: JSON.stringify({beer: beerDetails}),
-            headers: {
-                "Accept":"application/json",
-                "Content-Type":"application/json"
-            }
+  return dispatch => {
+    return fetch(`${API_URL}/beers`, {
+      method: 'POST',
+      body: JSON.stringify({beer: beerDetails}),
+      headers: {
+        "Accept":"application/json",
+        "Content-Type":"application/json"
+      }
+    })
+    .then(response => response.json())
+      .then(jsonresp => {
+        dispatch({
+          type: 'CREATE_BEER',
+          payload: jsonresp
         })
-            .then(resp => resp.json())
-            .then(jresp => {
-                dispatch({
-                    type: 'CREATE_BEER',
-                    payload: jresp
-                })
-            })
-            .catch((errors) => {
-                console.log(errors)
-            })
-    }
+      })
+      .catch((errors) => {
+        console.log(errors)
+    })
+  }
 }
-export function deleteBeer(beerId) {
-    return dispatch => {
-        return fetch(`${API_URL}/beers/${beerId}`, {
-            method: 'DELETE',
-            headers: {
-                "Accept":"application/json",
-                "Content-Type":"application/json"
-            }
+
+export function updateBeer(beerDetails) {
+  return dispatch => {
+    return fetch(`${API_URL}/beers/${beerDetails.id}`, {
+      method: 'PATCH',
+      body: JSON.stringify({beer: beerDetails}),
+      headers: {
+        "Accept":"application/json",
+        "Content-Type":"application/json"
+      }
+    })
+    .then(response => response.json())
+      .then(jsonresp => {
+        dispatch({
+          type: 'UPDATE_BEER',
+          payload: jsonresp
         })
-            .then(() => {
-                dispatch({
-                    type: 'DELETE_BEER',
-                    payload: beerId
-                })
-            })
-            .catch((errors) => {
-                console.log(errors)
-            })
-    }
+      })
+      .catch((errors) => {
+        console.log(errors)
+    })
+  }
+}
+
+export function deleteBeer(beerId) {
+  return dispatch => {
+    return fetch(`${API_URL}/beers/${beerId}`, {
+      method: 'DELETE',
+      headers: {
+        "Accept":"application/json",
+        "Content-Type":"application/json"
+      }
+    })
+      .then(() => {
+      dispatch({
+        type: 'DELETE_BEER',
+        payload: beerId
+      })
+    })
+    .catch((errors) => {
+    console.log(errors)
+    })
+  }
 }
