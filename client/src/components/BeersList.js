51cd4d4f1003a218a2e3b39ab6d7dff5459e9a70
github username <github email address>
2018-07-25 17:19:33 +0300

    different return depending on if existing beers, each beer is on a card

--- client/src/components/BeersList.js
+++ client/src/components/BeersList.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { Card, Col, Row } from 'antd';
 
 const BeersList = ({match, beers}) => {
-  return (
-    <div>
-      {beers.map(beer =>
-        <Link key={beer.id} to={`/beers/${beer.id}`}>
-          {beer.name} - {beer.style}
-          {beer.rating}
-        </Link>
-      )}
-    </div>
+  const savedBeers = (
+  <div style={{ background: '#ECECEC', padding: '30px' }}>
+    {beers.map(beer =>
+      <Row gutter={16}>
+        <Col span={8}>
+          <Card title={beer.name} extra={<Link key={beer.id} to={`/beers/${beer.id}`}>More Info</Link>} bordered={true}>
+            <p>{beer.style}</p>
+            <p>{beer.rating}</p>
+          </Card>
+        </Col>
+      </Row>
+    )}
+  </div>
+)
+const noBeers = (
+  <div>
+    <h1> You have no saved Beers </h1>
+    <h2><Link to="/beers/new">New Beer</Link> </h2>
+
+  </div>
+)
+return (
+  <div>
+    {beers.length !== 0 ? savedBeers : noBeers}
+  </div>
   )
 }
 
