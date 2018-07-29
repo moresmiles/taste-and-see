cb79130260b531b5deb891ef11bc444a16ce0616
github username <github email address>
2018-07-29 13:08:12 +0300

    account just shows user email for now

--- client/src/components/UserShow.js
+++ client/src/components/UserShow.js
@@ -1,31 +1,13 @@
-import React, { Component } from 'react';
-import { Link } from "react-router-dom";
-import { fetchBeers } from '../actions/beerActions';
-import { connect } from 'react-redux';
+import React from 'react';
 
- class UserShow extends Component {
-   constructor(props){
-     super(props)
-
-     this.state = {
-       beers: []
-     }
-   }
-
-   componentDidMount() {
-     this.props.fetchBeers();
-    }
-
- render(){
-   return (
+const UserShow = ({user}) => {
+  return (
       <div>
-        <h2>You are logged in YAY!</h2>
-        <p>ID: {user.id} </p>
-        <p>email: {user.email} </p>
-        <p><Link to="/new-beer">New Beer</Link> </p>
-        <p><Link to="/beer" >Saved Beers</Link> </p>
+        <h1> Your Account </h1>
+        <h2> Email: {user.email} </h2>
       </div>
     );
   }
-}
-export default connect(null, {fetchBeers})(UserShow);
+
+
+export default UserShow;
