bc46d0bdc782561efb69469b1e61f68e46d8dd36
github username <github email address>
2018-08-01 15:40:31 +0300

    changed style on welcome component

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -5,10 +5,10 @@ import { Button } from 'antd';
 const Welcome = ({match}) => {
 
   return (
-    <div className="welcome">
-      <h1>Taste and See</h1>
-      <h2>The place to store all the delicious beers that you've tried.</h2>
-      <h2>Please <Button type="normal" size="large"> <Link to="/signup" >Sign Up</Link></Button> or <Button type="normal" size="large"><Link to="/login" >Log In</Link></Button> to start </h2>
+    <div className="Welcome">
+      <p>Taste and See</p>
+      <p>The place to store all the delicious beers that you've tried.</p>
+      <p>Please <Button type="normal" size="large"> <Link to="/signup" >Sign Up</Link></Button> or <Button type="normal" size="large"><Link to="/login" >Log In</Link></Button> to start </p>
     </div>
   );
 }
