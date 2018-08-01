026abebada2664e69dabe2d0e7a3bc06c6cffd06
github username <github email address>
2018-08-01 16:10:48 +0300

    button design to BeersList component

--- client/src/components/BeersList.js
+++ client/src/components/BeersList.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import { Row, Col, Card } from 'antd';
+import { Row, Col, Card, Button } from 'antd';
 
 const BeersList = ({match, beers}) => {
   const savedBeers = (
-  <div className="beersList" >
+  <div>
       <Row style={{padding:'30px'}}>
         {beers.map(beer =>
         <Col style={{padding:'30px'}} span={8}>
@@ -20,11 +20,11 @@ const BeersList = ({match, beers}) => {
 const noBeers = (
   <div>
     <p> You have no saved Beers </p>
-    <p><Link to="/beers/new">New Beer</Link> </p>
+    <Button type="primary" size="large"><Link to="/beers/new">New Beer</Link></Button>
   </div>
 )
 return (
-  <div>
+  <div className="beersList">
     {beers.length !== 0 ? savedBeers : noBeers}
   </div>
   )
