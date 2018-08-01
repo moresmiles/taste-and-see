d1cb7d53ba949ae415cb1e057c580e81ffd142a6
github username <github email address>
2018-08-01 15:40:53 +0300

    added className to userShow component for CSS

--- client/src/components/UserShow.js
+++ client/src/components/UserShow.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const UserShow = ({user}) => {
   return (
-      <div>
-        <h1> Your Account </h1>
-        <h2> Email: {user.email} </h2>
+      <div className="userShow">
+        <p> Your Account </p>
+        <p> Email: {user.email} </p>
       </div>
     );
   }
